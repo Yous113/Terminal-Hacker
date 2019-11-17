404ed630731f854e2b4da0c80d42d21ca2c81891
Yous113 <44925411+Yous113@users.noreply.github.com>
2019-11-17 15:48:09 +0100

    Script 1

---
+++ Terminal Hacker/Assets/Scripts/Hacker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Terminal.WriteLine("Hello Commander");
+        Terminal.WriteLine("What would yoou like to hack into?");
+        Terminal.WriteLine("");
+        Terminal.WriteLine("Press 1 for the local libarary");
+        Terminal.WriteLine("Press 2 for NASA");
+        Terminal.WriteLine("Press 3 for the FBI");
+        Terminal.WriteLine("Enter your selection: ");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

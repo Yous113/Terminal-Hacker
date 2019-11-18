504e99be06c2cce71a938bc5c611dd0c9ece2d9f
Yous113 <44925411+Yous113@users.noreply.github.com>
2019-11-18 17:24:36 +0100

    script

--- Terminal Hacker/Assets/Scripts/Hacker.cs
+++ Terminal Hacker/Assets/Scripts/Hacker.cs
@@ -4,26 +4,31 @@ using UnityEngine;
 
 public class Hacker : MonoBehaviour
 {
-    public string greeting = "Hello Commander";
+    public string greeting = "Hello";
     
     // Start is called before the first frame update
     void Start()
     {
-        ShowMainMenu(greeting);
+        ShowMainMenu();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    void ShowMainMenu(string greet)
+    void ShowMainMenu()
     {
         Terminal.ClearScreen();
-        Terminal.WriteLine(greet);
         Terminal.WriteLine("What would yoou like to hack into?");
         Terminal.WriteLine("Press 1 for the local libarary");
         Terminal.WriteLine("Enter your selection: ");
+    }
+
+    void OnUserInput(string input)
+    {
+        if (input == "menu")
+        {
+            ShowMainMenu();
+        }
+        else
+        {
+            Terminal.WriteLine("Plese choose a valid level");
+        }
     }
 }

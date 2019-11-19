99aae614e6f7926f07c0753a25039e37b55335fd
Yous113 <44925411+Yous113@users.noreply.github.com>
2019-11-19 17:17:05 +0100

    script

--- Terminal Hacker/Assets/Scripts/Hacker.cs
+++ Terminal Hacker/Assets/Scripts/Hacker.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class Hacker : MonoBehaviour
 {
+    //Variables
     public string greeting = "Hello";
-    
+    public int level;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,18 +19,36 @@ public class Hacker : MonoBehaviour
         Terminal.ClearScreen();
         Terminal.WriteLine("What would yoou like to hack into?");
         Terminal.WriteLine("Press 1 for the local libarary");
+        Terminal.WriteLine("Press 2 for NASA");
         Terminal.WriteLine("Enter your selection: ");
     }
+
+    
 
     void OnUserInput(string input)
     {
         if (input == "menu")
         {
             ShowMainMenu();
+        } 
+        else if(input == "1")
+        {
+            level = 1;
+            StartGame();
+        }
+        else if (input == "2")
+        {
+            level = 2;
+            StartGame();
         }
         else
         {
             Terminal.WriteLine("Plese choose a valid level");
         }
+    }
+
+    void StartGame()
+    {
+        Terminal.WriteLine("You have chosen level " + level);
     }
 }
